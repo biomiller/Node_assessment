f2469b019ec1bd0023e5395eeae19e52200f8ec1
biomiller <92millero@gmail.com>
2019-07-26 13:25:53 +0100

    added item update

--- models/item.js
+++ models/item.js
@@ -8,6 +8,11 @@ const itemSchema = new Schema(
             type: String,
             required: true
         },
+        title:
+        {
+            type: String,
+            required: true
+        },
         content:
         {
             type: String,
--- routes/item.js
+++ routes/item.js
@@ -13,6 +13,10 @@ router.get("/test", (req, res) => {
     res.send("This is a test!");
 });
 
+
+// @route POST item/getItems
+// @desc check username and password then display all of the user's items
+// @access Public
 router.post("/getItems", (req, res) => {
     const errors = {};
 
@@ -20,32 +24,36 @@ router.post("/getItems", (req, res) => {
         .then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password)
-                .then(match => {
-                    if (match) {
-                        Item.find({ "username": req.body.username })
-                        .then(item => {
-                            if (!item) {
-                                errors.noItems = "Item not found";
-                                res.status(404).json(errors);
-                            }
-                            res.json(item);
-                        })
-                        
-                    } else {
-                        res.status(200).send("Incorrect password.")
-                    }
-                })
+                    .then(match => {
+                        if (match) {
+                            Item.find({ "username": req.body.username })
+                                .then(item => {
+                                    if (!item || !item.length) {
+                                        errors.noItems = "No items found.";
+                                        res.status(404).json(errors);
+                                    }
+                                    res.json(item);
+                                })
+
+                        } else {
+                            res.status(200).send("Incorrect password.")
+                        }
+                    })
             } else {
                 res.status(404).send("User not found.")
             }
         })
 });
 
+// @route POST item/createItems
+// @desc check username and password then create a new item
+// @access Public
 router.post("/createItem", (req, res) => {
     const errors = {};
 
     let newItem = new Item({
         username: req.body.username,
+        title: req.body.title,
         content: req.body.content
     });
 
@@ -53,20 +61,31 @@ router.post("/createItem", (req, res) => {
         .then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password)
-                .then(match => {
-                    if (match) {
-                        newItem.save()
-                        res.status(200).send("Added new Item")
-                    } else {
-                        res.status(200).send("Incorrect password.")
-                    }
-                })
+                    .then(match => {
+                        if (match) {
+                            Item.find({ "username": req.body.username, "title": req.body.title })
+                                .then(item => {
+                                    if (!item || !item.length) {
+                                        newItem.save()
+                                        res.status(200).send("Added new Item")
+                                    } else {
+                                        res.status(200).send("An item already exists with that title.")
+                                    }
+
+                                })
+                        } else {
+                            res.status(200).send("Incorrect password.")
+                        }
+                    })
             } else {
                 res.status(404).send("User not found.")
             }
         })
 });
 
+// @route DELETE item/deleteItems
+// @desc check username and password then delete oldest item
+// @access Public
 router.delete("/deleteItem", (req, res) => {
     const errors = {};
 
@@ -74,26 +93,30 @@ router.delete("/deleteItem", (req, res) => {
         .then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password)
-                .then(match => {
-                    if (match) {
-                        Item.deleteOne({ "username": req.body.username })
-                        .then((ok) => {
-                            if (ok.n == 0) {
-                                res.status(200).send("No items to delete.")
-                            } else {
-                                res.status(200).send("Item deleted.")
-                            }
-                        })
-                    } else {
-                        res.status(200).send("Incorrect password.")
-                    }
-                })
+                    .then(match => {
+                        if (match) {
+                            Item.deleteOne({ "username": req.body.username, "title": req.body.title })
+                                .then((ok) => {
+                                    if (ok.n == 0) {
+                                        res.status(200).send("No items exist with that title.")
+                                    } else {
+                                        res.status(200).send("Item deleted.")
+                                    }
+                                })
+                        } else {
+                            res.status(200).send("Incorrect password.")
+                        }
+                    })
             } else {
                 res.status(404).send("User not found.")
             }
         })
 });
 
+
+// @route DELETE item/deleteItems
+// @desc check username and password then delete all of the user's items
+// @access Public
 router.delete("/deleteAllItems", (req, res) => {
     const errors = {};
 
@@ -101,20 +124,55 @@ router.delete("/deleteAllItems", (req, res) => {
         .then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password)
-                .then(match => {
-                    if (match) {
-                        Item.deleteMany({ "username": req.body.username })
-                        .then((ok) => {
-                            if (ok.n == 0) {
-                                res.status(200).send("No items to delete.")
-                            } else {
-                                res.status(200).send("All Items deleted.")
-                            }
-                        })
-                    } else {
-                        res.status(200).send("Incorrect password.")
-                    }
-                })
+                    .then(match => {
+                        if (match) {
+                            Item.deleteMany({ "username": req.body.username })
+                                .then((ok) => {
+                                    if (ok.n == 0) {
+                                        res.status(200).send("No items to delete.")
+                                    } else {
+                                        res.status(200).send("All Items deleted.")
+                                    }
+                                })
+                        } else {
+                            res.status(200).send("Incorrect password.")
+                        }
+                    })
+            } else {
+                res.status(404).send("User not found.")
+            }
+        })
+});
+
+// @route DELETE item/deleteItems
+// @desc check username and password then delete oldest item
+// @access Public
+router.put("/updateItem", (req, res) => {
+    const errors = {};
+
+    User.findOne({ "username": req.body.username })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(match => {
+                        if (match) {
+                            Item.updateOne({ "username": req.body.username, "title": req.body.title },
+                                {
+                                    $set: {
+                                        "content": req.body.content,
+                                    }
+                                })
+                                .then((ok) => {
+                                    if (ok.n == 0) {
+                                        res.status(200).send("No items exist with that title.")
+                                    } else {
+                                        res.status(200).send("Item content updated.")
+                                    }
+                                })
+                        } else {
+                            res.status(200).send("Incorrect password.")
+                        }
+                    })
             } else {
                 res.status(404).send("User not found.")
             }
